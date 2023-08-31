c2e3da2da4b9d8781150612bb7600a149468c3b3
Jungkyu Yang <didwjd8590412@gmail.com>
2023-08-31 19:30:55 +0900

    feat: NotFoundPage 생성

--- src/pages/NotFoundPage/index.tsx
+++ src/pages/NotFoundPage/index.tsx
@@ -1,5 +1,31 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-export default function index() {
-	return <div>index</div>;
-}
+import styles from '../../utils/styles/NotFoundPage.module.scss';
+import { ROUTES } from '../../utils/constants/Routes';
+
+const NotFoundPage = () => {
+	const navigate = useNavigate();
+
+	const handleClick = (e: React.MouseEvent) => {
+		e.preventDefault();
+		navigate(`${ROUTES.MAIN}`);
+	};
+
+	return (
+		<div className={styles.container}>
+			<div className={styles.statusContainer}>
+				<div className={styles.status}>404</div>
+				<div className={styles.status}>404</div>
+			</div>
+			<div className={styles.contentContainer}>
+				<div className={styles.title}>현재 찾을 수 없는 페이지를 요청 하셨습니다.</div>
+				<button className={styles.button} onClick={handleClick}>
+					메인으로
+				</button>
+			</div>
+		</div>
+	);
+};
+
+export default NotFoundPage;
