29f445767924d6804980a8ab23fd94711d04a854
lyeejj <lyshine012@gmail.com>
2023-08-31 21:20:05 +0900

    feat: 메인페이지 구현

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,5 +1,12 @@
-import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { ROUTES } from '../../utils/constants/constants';
+import styles from './Main.module.scss';
 
 export default function MainPage() {
-	return <div>index</div>;
+	const navigate = useNavigate();
+	return (
+		<div className={styles.container}>
+			<button onClick={() => navigate(ROUTES.LIST)}>이슈리스트</button>
+		</div>
+	);
 }
